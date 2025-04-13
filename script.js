9fe7baf19d5f7dce5674a127a51c7ab060a0c4b4
ADEESH KRISHNA <161409954+adeeshkrishna@users.noreply.github.com>
2025-04-13 22:20:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+document.querySelectorAll(".work-card").forEach((card) => {
+  card.addEventListener("click", () => {
+    console.log("Work experience card clicked!");
+  });
+});
